550c01eb9c9248fc4e11c6fa35092ea92f0e6207
Vladimir Fibe <motiw@icloud.com>
2023-10-19 08:55:19 +0600

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Vladimir Fibe on 19.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
